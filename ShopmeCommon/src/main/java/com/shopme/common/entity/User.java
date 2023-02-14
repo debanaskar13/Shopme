e359003314a0627f586fb58625ed3e501206a1af
dna1301 <debashisnaskar1301@gmail.com>
2023-02-15 00:04:33 +0530

    Added Spring Security and modify User Module

--- ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -1,11 +1,12 @@
 package com.shopme.common.entity;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -116,15 +117,10 @@ public class User {
 		this.roles.add(role);
 	}
 
-	
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", email=" + email + ", firstName=" + firstName + ", lastName=" + lastName
 				+ ", roles=" + roles + "]";
 	}
 
-	
-	
-	
-	
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -42,6 +44,7 @@ public class UserController {
 		User user = new User();
 		user.setEnabled(true);
 		
+		model.addAttribute("pageTitle","Create New User");
 		model.addAttribute("user", user);
 		model.addAttribute("listRoles", listRoles);		
 		
@@ -57,5 +60,43 @@ public class UserController {
 		
 		return "redirect:/users";
 	}
+	
+	@GetMapping("/users/edit/{id}")
+	public String editUser(@PathVariable("id") Integer id,Model model, RedirectAttributes redirectAttributes) {
+		try {	
+			List<Role> listRoles = userService.listRoles();
+			User user = userService.get(id);
+			model.addAttribute("pageTitle","Edit User (ID: "+id+")");
+			model.addAttribute("user",user);
+			model.addAttribute("listRoles", listRoles);
+			return "user_form";
+		} catch (UserNotFoundException e) {
+			redirectAttributes.addFlashAttribute("message", e.getMessage());
+			return "redirect:/users";
+		}
+		
+	}
+	
+	@GetMapping("/users/delete/{id}")
+	public String delteUser(@PathVariable("id") Integer id,Model model, RedirectAttributes redirectAttributes) {
+		try {	
+			userService.delete(id);
+			redirectAttributes.addFlashAttribute("message","The user ID "+id+" has been deleted successfully");
+		} catch (UserNotFoundException e) {
+			redirectAttributes.addFlashAttribute("message", e.getMessage());
+		}
+		return "redirect:/users";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
