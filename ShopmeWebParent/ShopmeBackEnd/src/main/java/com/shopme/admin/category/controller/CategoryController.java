061600acbe7da915381cac518bdbc8289975e159
dna1301 <debashisnaskar1301@gmail.com>
2023-04-03 17:42:51 +0530

    added category image

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/controller/CategoryController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/controller/CategoryController.java
@@ -45,7 +45,7 @@ public class CategoryController {
         category.setImage(fileName);
 
         Category savedCategory = categoryService.save(category);
-        String uploadDir = "/category-images/"+savedCategory.getId();
+        String uploadDir = "../category-images/"+savedCategory.getId();
 
         FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
 
