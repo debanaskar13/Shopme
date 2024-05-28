3bdc3a5d3058d5defc4df86f4d2066ce91bd4ba0
debanaskar13 <debanaskar13@gmail.com>
2024-05-28 17:22:06 +0530

    Added User and Role Repository Unit tests

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -0,0 +1,68 @@
+package com.shopme.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = { "id", "description" })
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 40, nullable = false, unique = true)
+    private String name;
+    @Column(length = 150, nullable = false)
+    private String description;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Role other = (Role) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
---
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/user/UserRepositoryTest.java
@@ -0,0 +1,92 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testCreateNewUserWithOneRole() {
+        Role roleAdmin = entityManager.find(Role.class, 1);
+        User userDebashis = User.builder().email("debanaskar13@gmail.com").password("Deba1234@").firstName("Debashis")
+                .lastName("Naskar").build();
+        userDebashis.addRole(roleAdmin);
+
+        User savedUser = this.repository.save(userDebashis);
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testCreateNewUserWithTwoRole() {
+        Role roleEditor = entityManager.find(Role.class, 3);
+        Role roleAssistant = entityManager.find(Role.class, 5);
+        User userRavi = User.builder().email("ravi@gmail.com").password("Ravi@2024").firstName("Ravi")
+                .lastName("Kumar").build();
+        userRavi.addRole(roleEditor);
+        userRavi.addRole(roleAssistant);
+
+        User savedUser = this.repository.save(userRavi);
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testListAllUsers() {
+        List<User> listUsers = this.repository.findAll();
+        listUsers.forEach(user -> System.out.println(user));
+        assertThat(listUsers.size()).isGreaterThan(1);
+    }
+
+    @Test
+    public void testGetUserById() {
+        User userDebashis = this.repository.findById(1).get();
+        assertThat(userDebashis).isNotNull();
+    }
+
+    @Test
+    public void testUpdateUserDetails() {
+        User userDebashis = this.repository.findById(1).get();
+        userDebashis.setEnabled(true);
+        userDebashis.setEmail("Deba@gmail.com");
+
+        this.repository.save(userDebashis);
+    }
+
+    @Test
+    public void testUpdateUserRoles() {
+        User userRavi = this.repository.findById(2).get();
+        Role roleSalesperson = Role.builder().id(2).build();
+        Role roleEditor = Role.builder().id(3).build();
+
+        userRavi.getRoles().remove(roleEditor);
+        userRavi.getRoles().add(roleSalesperson);
+
+        this.repository.save(userRavi);
+
+    }
+
+    @Test
+    public void testDeleteUser() {
+        this.repository.deleteById(2);
+    }
+}
